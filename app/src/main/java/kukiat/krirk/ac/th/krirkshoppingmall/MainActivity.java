e1111d36975db1047be75d0ded13f49fe41bfc9b
Kukiat <Kukiat@gmail.com>
2018-03-06 11:43:53 +0700

    Before Lunch

--- app/src/main/java/kukiat/krirk/ac/th/krirkshoppingmall/MainActivity.java
+++ app/src/main/java/kukiat/krirk/ac/th/krirkshoppingmall/MainActivity.java
@@ -3,11 +3,19 @@ package kukiat.krirk.ac.th.krirkshoppingmall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kukiat.krirk.ac.th.krirkshoppingmall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+    } //Main Method
+} // Main Class
---
+++ app/src/main/java/kukiat/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
@@ -0,0 +1,23 @@
+package kukiat.krirk.ac.th.krirkshoppingmall.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kukiat.krirk.ac.th.krirkshoppingmall.R;
+
+/**
+ * Created by Cnz on 06/03/2018.
+ */
+
+public class MainFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
