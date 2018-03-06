77f1dc2420a846786fb90f1e1adbc7c17c1844d1
Kukiat <Kukiat@gmail.com>
2018-03-06 13:25:18 +0700

    Replace Fragment Success

--- app/src/main/java/kukiat/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/kukiat/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import kukiat.krirk.ac.th.krirkshoppingmall.R;
 
@@ -14,6 +15,36 @@ import kukiat.krirk.ac.th.krirkshoppingmall.R;
  */
 
 public class MainFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+
+    } // Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+
+            }
+        });
+
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
