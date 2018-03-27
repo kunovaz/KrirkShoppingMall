0f92028af4f32e4c162fdac4cf1be30aa88898c4
Kukiat <Kukiat@gmail.com>
2018-03-27 14:44:41 +0700

    Authen OK

--- app/src/main/java/kukiat/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/kukiat/krirk/ac/th/krirkshoppingmall/fragment/MainFragment.java
@@ -3,15 +3,22 @@ package kukiat.krirk.ac.th.krirkshoppingmall.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import kukiat.krirk.ac.th.krirkshoppingmall.R;
+import kukiat.krirk.ac.th.krirkshoppingmall.utility.GetAllDataByURL;
 import kukiat.krirk.ac.th.krirkshoppingmall.utility.MyAlert;
+import kukiat.krirk.ac.th.krirkshoppingmall.utility.MyConstant;
 
 /**
  * Created by Cnz on 06/03/2018.
@@ -49,7 +56,62 @@ public class MainFragment extends Fragment {
                             getString(R.string.message_have_space));
                 } else {
 //                    No Space
-                }
+                    try {
+
+                        MyConstant myConstant = new MyConstant();
+                        boolean statusAboolean = true;
+                        GetAllDataByURL getAllDataByURL = new GetAllDataByURL(getActivity());
+                        getAllDataByURL.execute(myConstant.getUrlGetAllUserString());
+
+                        String jsonString = getAllDataByURL.get();
+                        Log.d("27MarchV1", "JSON ==> " + jsonString);
+
+                        JSONArray jsonArray = new JSONArray(jsonString);
+
+                        String[] columnStrings = myConstant.getColumnUser();
+                        String[] loginStrings = new String[columnStrings.length];
+
+                        for (int i=0; i<jsonArray.length(); i+=1) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            if (userString.equals(jsonObject.getString(columnStrings[2]))) {
+                                statusAboolean = false;
+                                for (int i1=0; i1<columnStrings.length; i1+=1) {
+                                    loginStrings[i1] = jsonObject.getString(columnStrings[i1]);
+                                }
+
+                            } // if
+
+
+                        } // for
+
+                        if (statusAboolean) {
+//                            User False
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("User False",
+                                    "No This User in my Database");
+
+
+                        } else if (passwordString.equals(loginStrings[3])) {
+//                            Password True
+                            Toast.makeText(getActivity(), "Wellcome " + loginStrings[1],
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+//                            Password False
+                            MyAlert myAlert = new MyAlert(getActivity());
+                            myAlert.myDialog("Password False",
+                                    "Please Try Again Password False");
+                        }
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
+
+                } // if
 
 
             }
